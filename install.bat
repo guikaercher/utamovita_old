476ee9ad67b9755ecae843e6ae13090b7cebcb6f
Guilherme Kaercher <contato@guilhermekaercher.com.br>
2018-12-25 21:40:57 -0200

    added .bat to install everything automatically

---
+++ install.bat
@@ -0,0 +1,6 @@
+START /WAIT installation_windows_scripts\install_choco.bat
+
+start /WAIT installation_windows_scripts\install_node.bat
+start /WAIT installation_windows_scripts\install_python.bat
+
+start /WAIT installation_windows_scripts\install_app.bat
